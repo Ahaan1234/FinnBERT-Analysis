4ce6bda9021d114da867f0d01b834002a5109725
Ahaan1234 <1ahaanshah@gmail.com>
2026-07-05 12:24:18 +0530

    retrieval using eodhd

---
+++ ahaan-code/content-retriever.py
@@ -0,0 +1,26 @@
+# given a ticker, call on the EODHD API to receive a json. 
+# Store this json to avoid overusing limit free api calls xP
+# from "content" print the first 5 words of each summary. 
+
+import requests
+
+API_TOKEN = "demo"
+ticker = "AAPL.US"
+
+params = {
+    "s": ticker,
+    "from": "2024-08-01",
+    "to": "2025-07-28",
+    "limit":100,
+    "api_token": API_TOKEN,
+    "fmt": "json"
+}
+
+r = requests.get("https://eodhd.com/api/news", params=params, timeout=30)
+r.raise_for_status()
+articles = r.json()
+
+count = 1
+for article in articles:
+    print(f"{count}. title: ", article["title"], "\nexcerpt: ", article["content"][:30])
+    count+=1
